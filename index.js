858066e5fb5534fbe8b6cfd9bcf8a07e8cd151d5
Kante <esemeudo372@gmail.com>
2025-01-13 17:23:46 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// let button = document.getElementById('firstbut')
+
+
+
+
+      
+    //   let randomNO = Math.random();
+    //   //  alert(randomNO)
+    //    AIMove = '';
+       
+    //   if (randomNo >= 1/3 && randomNo < 2/3) {
+    //         AIMove ='paper';
+    //  } else if (randomNo >= 2/3 && randomNo < 1) {
+    //    AIMove ='scissors';
+    //  } else if  (randomNo >= 0 && randomNo < 1/3 ) {
+    //   AIMove = 'rock';
+    //  }
+    //   result = '';
+
+    //   console.log(AIMove);
+  
+
+
